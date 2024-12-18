59b39fb6d8921bfc957ae017637584056b99a016
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-12-18 12:21:34 +0300

    fix: delete comments

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,22 +1,22 @@
-// import { Advantages } from 'widgets/Advantages';
-// import { CallForm } from 'widgets/CallForm';
-// import { Categories } from 'widgets/Categories/Categories';
-// import { Header } from 'widgets/Header';
-// import { Hero } from 'widgets/Hero';
-// import { PopularProducts } from 'widgets/PopularProducts';
-// import { Statistics } from 'widgets/Statistics';
-// import styles from './HomePage.module.scss';
+import { Advantages } from 'widgets/Advantages';
+import { CallForm } from 'widgets/CallForm';
+import { Categories } from 'widgets/Categories/Categories';
+import { Header } from 'widgets/Header';
+import { Hero } from 'widgets/Hero';
+import { PopularProducts } from 'widgets/PopularProducts';
+import { Statistics } from 'widgets/Statistics';
+import styles from './HomePage.module.scss';
 
 export function HomePage() {
   return (
     <>
-      {/* <Header /> */}
-      {/* <Hero /> */}
-      {/* <Statistics /> */}
-      {/* <Advantages className={styles.HomePage__Advantages} /> */}
-      {/* <Categories className={styles.HomePage__Categories} /> */}
-      {/* <PopularProducts className={styles.HomePage__PopularProducts}/> */}
-      {/* <CallForm /> */}
+      <Header />
+      <Hero />
+      <Statistics />
+      <Advantages className={styles.HomePage__Advantages} />
+      <Categories className={styles.HomePage__Categories} />
+      <PopularProducts className={styles.HomePage__PopularProducts} />
+      <CallForm />
     </>
   );
 }
